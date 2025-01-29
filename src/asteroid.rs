414262a29b0570b9108e52d78d23b489919f6596
Andydandy21 <a.smithb21@gmail.com>
2025-01-29 06:58:58 -0600

    add random spawn location

--- src/asteroid.rs
+++ src/asteroid.rs
@@ -1,4 +1,5 @@
-use bevy::{color::palettes::css::RED, prelude::*};
+use bevy::{color::palettes::css::RED, prelude::*, window::PrimaryWindow};
+use rand::Rng;
 
 pub struct AsteroidPlugin;
 
@@ -12,9 +13,22 @@ fn spawn_asteroid(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    window_q: Query<&Window, With<PrimaryWindow>>,
 ) {
+    let mut rng = rand::rng();
+    let window = window_q.single();
+    let half_width = window.width() / 2.0;
+    let half_height = window.height() / 2.0;
+
+    let rand_x = rng.random_range(-half_width..half_width);
+    let rand_y = rng.random_range(-half_height..half_height);
+
     commands.spawn((
         Mesh2d(meshes.add(Circle::new(25.0))),
         MeshMaterial2d(materials.add(ColorMaterial::from_color(RED))),
+        Transform {
+            translation: Vec3::new(rand_x, rand_y, 0.0),
+            ..Default::default()
+        },
     ));
 }
